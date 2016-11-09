bd7750bce04750c73a5ff96ed72553c288fe6d4e
Anthony Kuske <anthonykuske@gmail.com>
2016-11-09 21:08:11 +0000

    Add order by support

--- src/Engines/MySQLEngine.php
+++ src/Engines/MySQLEngine.php
@@ -58,6 +58,10 @@ class MySQLEngine extends Engine
 
         $result['count'] = $query->count();
 
+        foreach ($builder->orders as $order) {
+            $query->orderBy($order['column'], $order['direction']);
+        }
+
         if($builder->limit) {
             $query = $query->take($builder->limit);
         }
