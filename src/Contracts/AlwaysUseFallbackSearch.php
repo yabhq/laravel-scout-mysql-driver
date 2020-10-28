af4bc229266f2e1b4ff74f13575e24cfaf91ed4a
Mr. Jingles <er_po@web.de>
2020-10-28 09:03:59 +0100

    adding default fallback option for model

---
+++ src/Contracts/AlwaysUseFallbackSearch.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Yab\MySQLScout\Contracts;
+
+/**
+ * Implement this interface if the fallback search should be used for the model
+ *
+ */
+interface AlwaysUseFallbackSearch
+{
+}
