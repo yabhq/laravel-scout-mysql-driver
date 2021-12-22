c9d902d90ed855e3cf2d636a86944f202fa864db
Gabriel Diez <gabrieldiez@hotmail.com>
2021-12-22 12:20:51 -0300

    Fix where() not working

--- src/Engines/MySQLEngine.php
+++ src/Engines/MySQLEngine.php
@@ -65,7 +65,6 @@ class MySQLEngine extends Engine
 
         $whereRawString = $mode->buildWhereRawString($builder);
         $params = $mode->buildParams($builder);
-        ksort($params,SORT_NATURAL);
 
         $model = $builder->model;
         $query = $model::whereRaw($whereRawString, $params);
